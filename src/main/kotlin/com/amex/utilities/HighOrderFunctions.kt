0417f3af14d49abe8c852d0d349dfb306d38964a
parameswari.bala <parameswaribala@gmail.com>
2023-12-06 16:01:50 +0530

    updated

--- src/main/kotlin/com/amex/utilities/HighOrderFunctions.kt
+++ src/main/kotlin/com/amex/utilities/HighOrderFunctions.kt
@@ -1,8 +1,15 @@
 package com.amex.utilities
 
+import com.amex.models.Address
+import com.amex.models.Gender
+import com.amex.models.Individual
+import java.time.LocalDate
 
-fun validate(data:String,operation:(String)->Boolean): Boolean{
-    return operation(data)
+
+fun validate(data:String,individual1:Individual,individual2: Individual,
+             operation1:(String)->Boolean,
+             operation2:(Individual,Individual)->Boolean): Boolean{
+    return operation1(data) && operation2(individual1,individual2)
 }
 
 fun validateAccountNo(accountNo:String) : Boolean{
@@ -14,11 +21,16 @@ fun validateName(name: String): Boolean{
     return pattern.matches(name)
 }
 
+fun compareInstances(individual1:Individual,individual2: Individual):Boolean{
+    return individual1.dob.isBefore(individual2.dob)
+}
 
 fun main() {
+    var individual1=Individual(1353246, Address(),"Bala", Gender.FEMALE, LocalDate.of(1970,12,2))
+    var individual2=Individual(1353289, Address(),"Manickam", Gender.MALE, LocalDate.of(1995,12,7))
 
     //function passed as parameter
-    validate("123456789012345324",::validateAccountNo)
+    println(validate("123456789012345324",individual1,individual2,::validateAccountNo, ::compareInstances))
 
-    validate("Parameswari",::validateName)
+    println(validate("Parameswari",individual1,individual2,::validateName,::compareInstances))
 }
