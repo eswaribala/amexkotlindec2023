738eb43ae6c8b295354b592114caef78dc6e21d0
parameswari.bala <parameswaribala@gmail.com>
2023-12-06 13:11:44 +0530

    updated

--- src/main/kotlin/com/amex/utilities/App.kt
+++ src/main/kotlin/com/amex/utilities/App.kt
@@ -7,7 +7,7 @@ import com.amex.models.Member
 fun main(){
 
     //named arguments, primary constructor
-   var customer= Customer(_accountNo=122343534,"Param","param@gmail.com",_contactNo=9952032862, _address = Address())
+  // var customer= Customer(_accountNo=122343534,"Param","param@gmail.com",_contactNo=9952032862, _address = Address())
 
   //  var formatter=String.format("%s,%s",customer.name,customer.email)
    // println("$formatter")
@@ -17,21 +17,21 @@ fun main(){
     formatter=String.format("%s,%s",customerObj.name,customerObj.email)
     println("$formatter")*/
 
- var customerObj=Customer(34576769769, Address());
+// var customerObj=Customer(34576769769, Address());
    // formatter=String.format("%s,%s",customerObj.name,customerObj.email)
     //println("$formatter")
 
-    var customerObj1=Customer(34576769769, Address());
+  //  var customerObj1=Customer(34576769769, Address());
    // formatter=String.format("%s,%s",customerObj1.name,customerObj1.email)
    // println("$formatter")
     //static variable
-    println("Counter=${Customer.counter}")
+   // println("Counter=${Customer.counter}")
     //instance method
-    println("Counter=${customerObj1.showCounter()}")
+    //println("Counter=${customerObj1.showCounter()}")
     //static method
-    println("Counter=${Customer.showData()}")
+    //println("Counter=${Customer.showData()}")
 
-    var customerobj2=Customer()
+    //var customerobj2=Customer()
 
     //test data class
 
