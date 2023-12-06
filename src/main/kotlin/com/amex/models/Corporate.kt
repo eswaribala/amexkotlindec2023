83e218cd7341c3ce1ac18775e086e207df0f7033
parameswari.bala <parameswaribala@gmail.com>
2023-12-06 11:17:08 +0530

    updated

--- src/main/kotlin/com/amex/models/Corporate.kt
+++ src/main/kotlin/com/amex/models/Corporate.kt
@@ -1,6 +1,7 @@
 package com.amex.models
 
-class Corporate(_accountNo:Long,_address:Address,_companyType:CompanyType):Customer(_accountNo,_address) {
+class Corporate(_accountNo:Long,_name:String,_email:String,_password:String,_contactNo:Long,_address:Address,_companyType:CompanyType):
+    Customer(_accountNo,_name,_email,_password,_contactNo,_address) {
 
     var companyType=_companyType
 }
