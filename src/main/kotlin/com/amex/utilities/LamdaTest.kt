56cf00f5e1e60645fba5965a0f64442314ec5ad6
parameswari.bala <parameswaribala@gmail.com>
2023-12-06 15:15:04 +0530

    updated

--- src/main/kotlin/com/amex/utilities/LamdaTest.kt
+++ src/main/kotlin/com/amex/utilities/LamdaTest.kt
@@ -6,23 +6,17 @@ import com.amex.models.Individual
 import java.time.LocalDate
 
 
-fun invokeLambda(individual1: Individual, individual2:Individual): (Individual, Individual) -> Boolean {
+fun  main() {
 
     val find=fun(individual1: Individual, individual2:Individual): Boolean{
 
         return individual1.dob.isBefore(individual2.dob)
 
     }
-    return find
-
-
-}
-
-fun  main() {
 
     var individual1=Individual(1353246, Address(),"Bala", Gender.FEMALE, LocalDate.of(1970,12,2))
     var individual2=Individual(1353289, Address(),"Manickam", Gender.MALE, LocalDate.of(1995,12,7))
-    val result= invokeLambda(individual1,individual2)
+    val result= find(individual1,individual2)
 
     println("The result=${result}")
 }
