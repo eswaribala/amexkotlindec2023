7a3fbfd473ce793bd48fd8fdbe6404e3c8557fc8
parameswari.bala <parameswaribala@gmail.com>
2023-12-07 13:15:30 +0530

    updated

--- src/main/kotlin/com/amex/utilities/SetDemo.kt
+++ src/main/kotlin/com/amex/utilities/SetDemo.kt
@@ -7,11 +7,31 @@ fun main(){
     var idSets= mutableSetOf<Long>()
 
     for(id in 1..100){
-        idSets.add(Random.nextInt(1,50).toLong())
+        idSets.add(Random.nextInt(101,150).toLong())
     }
 
     idSets
         .sorted()
         .forEach{it-> println("${it}")}
 
+
+    var javaDevelopers= setOf<String>("Ashu","Sid","Priya")
+    var kotlinDevelopers= setOf<String>("Sid","Charu","VIgnesh")
+
+    //list java and kotlin developers
+    println("Java and Kotlin Developers")
+    javaDevelopers.intersect(kotlinDevelopers).forEach{
+            it-> println("${it}")
+    }
+    println("Java Developers")
+    //list only java developers
+    javaDevelopers.minus(kotlinDevelopers).forEach{
+            it-> println("${it}")
+    }
+
+    println("All Developers")
+    //list all the developers
+    javaDevelopers.union(kotlinDevelopers).forEach{
+            it-> println("${it}")
+    }
 }
