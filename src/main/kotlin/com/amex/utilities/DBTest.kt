cdd8e98baa6e0698787d67eccb2587d129ee22c8
parameswari.bala <parameswaribala@gmail.com>
2023-12-07 12:29:42 +0530

    updated

--- src/main/kotlin/com/amex/utilities/DBTest.kt
+++ src/main/kotlin/com/amex/utilities/DBTest.kt
@@ -1,8 +1,21 @@
 package com.amex.utilities
 
 import com.amex.dao.IndividualSubSystem
+import com.amex.models.Address
+import com.amex.models.Gender
+import com.amex.models.Individual
+import java.time.LocalDate
+import kotlin.random.Random
 
 fun main() {
 
     var individualSubSystem=IndividualSubSystem()
+
+    for(records in 1..100)
+       individualSubSystem.addIndividual(Individual(Random.nextInt(100000,100000000).toLong(), Address(),
+        "Bala", Gender.FEMALE, LocalDate.of(Random.nextInt(1960,2022),Random.nextInt(1,12),
+               Random.nextInt(1,26))))
+
+    individualSubSystem.getAllIndividuals().forEach{it->println("${it.accountNo},${it.dob.toString()}")}
+
 }
