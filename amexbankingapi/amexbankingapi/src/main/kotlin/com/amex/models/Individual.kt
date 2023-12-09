722c705f052be72b91a0ea50862d6c5917916ac3
parameswari.bala <parameswaribala@gmail.com>
2023-12-09 11:06:00 +0530

    updated

--- amexbankingapi/amexbankingapi/src/main/kotlin/com/amex/models/Individual.kt
+++ amexbankingapi/amexbankingapi/src/main/kotlin/com/amex/models/Individual.kt
@@ -1,15 +1,23 @@
 package com.amex.models
 
 
+import kotlinx.serialization.Contextual
 import java.time.LocalDate
+@kotlinx.serialization.Serializable
+class Individual:Customer{
+    var surName:String
+    var gender:Gender
+    @Contextual
+    var dob:LocalDate
+    constructor(_accountNo:Long,_address:Address,_surName:String, _gender: Gender,_dob:LocalDate){
 
-class Individual(_accountNo:Long,_address:Address,_surName:String, _gender: Gender,_dob:LocalDate):Customer(_accountNo,_address) {
-
+        surName=_surName;
+        gender=_gender
+        dob=_dob
+    }
     //override variable
    // override val code="CI"
 
-        var surName=_surName;
-        var gender=_gender
-        var dob=_dob
+
 
 }
