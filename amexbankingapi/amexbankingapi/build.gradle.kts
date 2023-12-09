b5a8f4e150805b23c0ec0479fb6f39535bf39a0a
parameswari.bala <parameswaribala@gmail.com>
2023-12-09 11:51:11 +0530

    updated

--- amexbankingapi/amexbankingapi/build.gradle.kts
+++ amexbankingapi/amexbankingapi/build.gradle.kts
@@ -29,6 +29,13 @@ dependencies {
     implementation("io.ktor:ktor-server-openapi")
     implementation("io.ktor:ktor-server-swagger-jvm")
     implementation("io.github.smiley4:ktor-swagger-ui:1.0.1")
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-datetime-jvm
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.5.0")
+// https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-serialization-runtime
+   // implementation("org.jetbrains.kotlinx", "kotlinx-serialization-runtime", "1.0-M1-SNAPSHOT")
+// https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-serialization-runtime
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:1.0-M1-1.4.0-rc")
+
     // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
     implementation("com.mysql:mysql-connector-j:8.1.0")
     implementation("io.ktor:ktor-server-call-logging-jvm")
--- amexbankingapi/amexbankingapi/src/main/kotlin/com/amex/models/Individual.kt
+++ amexbankingapi/amexbankingapi/src/main/kotlin/com/amex/models/Individual.kt
@@ -1,14 +1,22 @@
 package com.amex.models
 
 
-import kotlinx.serialization.Contextual
+import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
 import java.time.LocalDate
 import java.util.*
-
+object DateSerializer : KSerializer<Date> {
+    override val descriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.LONG)
+    override fun serialize(encoder: Encoder, value: Date) = encoder.encodeLong(value.time)
+    override fun deserialize(decoder: Decoder): Date = Date(decoder.decodeLong())
+}
 @kotlinx.serialization.Serializable
 data class Individual(var _accountNo:Long,var _address:Address,var _surName:String, var _gender: Gender,
-                       var _dob: String
-)
+                      @Serializable(DateSerializer::class) var _dob: Date):Customer(_accountNo,_address)
     //override variable
    // override val code="CI"
 
