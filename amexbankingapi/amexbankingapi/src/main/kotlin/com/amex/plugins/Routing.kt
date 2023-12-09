2d3e27ff2c7a756da51101ce71d7cf5e8ec5dc24
parameswari.bala <parameswaribala@gmail.com>
2023-12-09 10:18:54 +0530

    updated

--- amexbankingapi/amexbankingapi/src/main/kotlin/com/amex/plugins/Routing.kt
+++ amexbankingapi/amexbankingapi/src/main/kotlin/com/amex/plugins/Routing.kt
@@ -6,8 +6,20 @@ import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/customers") {
-            call.respondText("Loading Customers.....")
+        route("/customers"){
+            get {
+                call.respondText("Loading Customers.....")
+            }
+            post {
+                call.respondText("Sending Customers.....")
+            }
+            put {
+                call.respondText("Updating Customers.....")
+            }
+            delete {
+                call.respondText("Deleting Customers.....")
+            }
         }
+
     }
 }
