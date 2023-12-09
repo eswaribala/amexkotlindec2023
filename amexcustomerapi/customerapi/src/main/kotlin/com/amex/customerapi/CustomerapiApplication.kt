937c68d8dc4cf8dc2db9dfdf814a613d7c267995
parameswari.bala <parameswaribala@gmail.com>
2023-12-09 12:46:38 +0530

    updated

---
+++ amexcustomerapi/customerapi/src/main/kotlin/com/amex/customerapi/CustomerapiApplication.kt
@@ -0,0 +1,11 @@
+package com.amex.customerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CustomerapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<CustomerapiApplication>(*args)
+}
